7f776c4209f871de0c84fba9122db291f7b40809
Dave <dave@fogmine.com>
2019-09-18 21:22:09 -0400

    cleaned up the connect program to have better formatted output

--- connect.py
+++ connect.py
@@ -1,24 +1,37 @@
 import serial.tools.list_ports
+import time
+from pyfirmata import Arduino, util
+
 ports = list(serial.tools.list_ports.comports())
 
-print("Available USB devices:")
+if len(ports) == 0:
+    raise Exception("No ports found!")
+
+print("\nAvailable USB ports:\n")
+print("Port Number\tPort Name")
 
 for idx, p in enumerate(ports):
-    print(idx, p.device)
+    print(f"{idx}\t\t({p.device})")
 
-port_num = int(input("Please enter a device number to connect to: "))
+port_num = int(input(f"Please enter the number for the Arduino port [0-{len(ports)-1}]: "))
 
 LED_PIN = 13
 
-import time
-from pyfirmata import Arduino, util
+print("\nConnecting to the Arduino...")
+
 board = Arduino(ports[port_num].device)
 
-for n in range(10):
+print("Connected!\n")
+
+for n in range(4):
     board.digital[LED_PIN].write(1)
+    print(f"Light is: ON ", end="\r")
     
     time.sleep(1)
     
     board.digital[LED_PIN].write(0)
+    print("Light is: OFF", end="\r")
     
     time.sleep(1)
+
+print('\nDone!')
