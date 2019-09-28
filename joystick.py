964fe157888f675e1e5335d8be9823b657a11986
Dave <dave@fogmine.com>
2019-09-27 20:39:15 -0400

    scaled down the joystick code significantly and made shutdown smoother

--- joystick.py
+++ joystick.py
@@ -1,87 +1,119 @@
 import threading
 import asyncio
 from pymata_express.pymata_express import PymataExpress
+from collections import namedtuple
 
-# Constants
-SW_PIN = 2  # Switch digital output
-X_PIN = 0  # X analog output
-Y_PIN = 1  # X analog output
-CENTER_POINT = 512  # Value of an axis (X or Y) when joystick is centered
-DIFFERENTIAL = 10  # Changes of +/- this value required to trigger callback, reduces noise
+Bound = namedtuple('Bound', 'lower upper')
+
+# Switch digital input
+SW_PIN = 2
+
+# X analog input
+X_PIN = 0
+
+# Y analog input
+Y_PIN = 1
+
+# Changes of +/- this value required to trigger callback, reduces noise
+DIFFERENTIAL = 10
+
+# bounds for x value to be considered BACK direction
+X_BACK_BOUNDS = Bound(0, 500)
+
+# bounds for x value to be considered FORWARD direction
+X_FORWARD_BOUNDS = Bound(540, 1022)
+
+# bounds for y value to be considered DOWN direction
+Y_UP_BOUNDS = Bound(0, 500)
+
+# bounds for y value to be considered UP direction
+Y_DOWN_BOUNDS = Bound(520, 1022)
 
 # Hold last read pin values in storage. Values are keyed by the pin number.
+# TODO need better (threadsafe) storage mechanism
 storage = {X_PIN: 0, Y_PIN: 0, SW_PIN: 0}
 
 
-async def input_handler(data):
+async def x_handler(data):
     """
-    A callback function to report switch changes.
+    An async callback function to report x input pin changes.
     :param data: [pin, current reported value, pin_mode, timestamp]
     """
     val = 0
 
-    if data[0] == X_PIN:
-        if data[1] < 500:
-            val = -1
-        elif data[1] > 540:
-            val = 1
-        storage[data[0]] = val
+    if data[1] < X_BACK_BOUNDS.upper:
+        val = -1
+    elif data[1] > X_FORWARD_BOUNDS.lower:
+        val = 1
 
-    elif data[0] == Y_PIN:
-        if data[1] < 500:
-            val = 1
-        elif data[1] > 520:
-            val = -1
-        storage[data[0]] = val
-
-    elif data[0] == SW_PIN:
-        storage[data[0]] = 1 - data[1]
+    storage[data[0]] = val
 
 
-async def analog_read(board, pin):
-    # Use a differential to avoid noise fluctuations on the analog
-    await board.set_pin_mode_analog_input(pin, callback=input_handler,
-                                          differential=DIFFERENTIAL)
+async def y_handler(data):
+    """
+    An async callback function to report y input pin changes.
+    :param data: [pin, current reported value, pin_mode, timestamp]
+    """
+    val = 0
 
-    t = threading.current_thread()
-    while getattr(t, 'do_run', True):
-        await asyncio.sleep(1)
+    # Y axis is inverted, e.g. lower numbers = UP
+    if data[1] < Y_UP_BOUNDS.upper:
+        val = 1
+    elif data[1] > Y_DOWN_BOUNDS.lower:
+        val = -1
+    storage[data[0]] = val
 
 
-async def digital_pullup_read(board, pin):
-    await board.set_pin_mode_digital_input_pullup(pin, callback=input_handler)
-
-    t = threading.current_thread()
-    while getattr(t, 'do_run', True):
-        await asyncio.sleep(1)
+async def switch_handler(data):
+    """
+    An async callback function to report switch changes.
+    :param data: [pin, current reported value, pin_mode, timestamp]
+    """
+    # the switch goes to 0 when pressed, we flip it so that
+    # pressed is 1 and not pressed is 0
+    storage[data[0]] = 1 - data[1]
 
 
 async def print_vals():
     t = threading.current_thread()
     while getattr(t, 'do_run', True):
         print(f'X-Axis: {storage[X_PIN]}, Y-Axis: {storage[Y_PIN]}, '
-              f'Switch: {storage[SW_PIN]}     ', end='\r')
+                f'Switch: {storage[SW_PIN]}     ', end='\r')
         await asyncio.sleep(0.1)
+    asyncio.get_event_loop.stop()
 
 
-def main(event_loop=None):
+def main(event_loop, cb=None):
     """
     Processes the joystick analog and digital inputs
-    :param event_loop: an optional event loop to use for execution
+    :param event_loop: the event loop to use for execution
+    :param cb: an optional callback lop to use
     """
     asyncio.set_event_loop(event_loop)
     board = PymataExpress()
 
     try:
-        tasks = asyncio.gather(
-                analog_read(board, X_PIN),
-                analog_read(board, Y_PIN),
-                digital_pullup_read(board, SW_PIN),
-                print_vals())
-        event_loop.run_until_complete(tasks)
+        pin_setup_task = asyncio.gather(
+                board.set_pin_mode_analog_input(X_PIN, callback=x_handler,
+                                                differential=DIFFERENTIAL),
+                board.set_pin_mode_analog_input(Y_PIN, callback=y_handler,
+                                                differential=DIFFERENTIAL),
+                board.set_pin_mode_digital_input_pullup(SW_PIN,
+                                                        callback=switch_handler)
+        )
+
+        event_loop.run_until_complete(pin_setup_task)
+
+        reporting_task = asyncio.ensure_future(print_vals())
+
+        # Now loop forever printing the recorded values
+        # from the handlers (callbacks)
+        event_loop.run_until_complete(reporting_task)
+
     except KeyboardInterrupt:
-        tasks.cancel()
+        event_loop.run_until_complete(board.shutdown())
+        reporting_task.cancel()
 
 
 if __name__ == '__main__':
-    main(loop=asyncio.get_event_loop())
+    main(asyncio.get_event_loop())
